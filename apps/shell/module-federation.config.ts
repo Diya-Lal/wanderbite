773868b9b04cc7c9892b769abf71cb393b719a8b
Diya-Lal <diyalal.a@gmail.com>
2026-03-10 21:53:51 +0100

    deploy to netlify

--- apps/shell/module-federation.config.ts
+++ apps/shell/module-federation.config.ts
@@ -1,8 +1,13 @@
 import { ModuleFederationConfig } from '@nx/module-federation';
 
+const isProduction = process.env['NODE_ENV'] === 'production';
+
 const config: ModuleFederationConfig = {
   name: 'shell',
-  remotes: ['homepage', 'destinations'],
+  remotes: [
+    ['homepage',     isProduction ? 'https://wanderbite-homepage.netlify.app'     : 'http://localhost:4201'],
+    ['destinations', isProduction ? 'https://wanderbite-destinations.netlify.app' : 'http://localhost:4202'],
+  ],
 };
 
 export default config;
